ab996addef53886afca8b24430bd886fdc23acfd
Simone Gazza <gazza.sim@gmail.com>
2020-12-28 09:11:30 +0100

    Design by

---
+++ src/elenanironi/ElenaNironi.js
@@ -0,0 +1,11 @@
+import "./ElenaNironi.css"
+
+const ElenaNironi = props => { 
+    return (
+        <span className="elenaNironi">
+            <p className="elenaNironiP">Design by </p><a href="http://www.linkedin.com/in/elena-n-0a2798a0">Elena Nironi</a>
+        </span>
+    )
+}
+
+export default ElenaNironi
