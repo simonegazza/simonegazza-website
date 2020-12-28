7fc9a9c14e01c6b98c3075f24aa43a81082519d7
Simone Gazza <gazza.sim@gmail.com>
2020-12-28 09:09:01 +0100

    Name section

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+
+import BackgroundAnimation from "./background/Background"
+import Name from "./name/Name"
+import Profile from "./profile/Profile"
+import Information from "./information/Information"
+import ElenaNironi from "./elenanironi/ElenaNironi"
+
+import "./App.css"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <div>
+      <div className="background"></div>
+      <BackgroundAnimation />
+    <div className="mobileApp">
+        <div className="hi">
+          <Name />
+        </div>
+        <div className="app">
+          <Profile />
+          <Information />
+        </div>
+        <ElenaNironi />
     </div>
-  );
+  </div>
+  )
 }
 
-export default App;
+export default App
